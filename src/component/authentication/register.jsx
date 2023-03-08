b3040ede1d7f383156e9aa224266b6a21a29d7a3
hamedGhaffari <hamedghaffari555@gmail.com>
2023-03-08 01:27:39 -0800

    update

--- src/component/authentication/register.jsx
+++ src/component/authentication/register.jsx
@@ -47,7 +47,7 @@ function Register() {
                         src={LOGO}
                         alt="Workflow"
                     />
-                    <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">  DDEM ثبت نام در</h2>
+                    <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">   ثبت نام در DDEM</h2>
                 </div>
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                     <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
